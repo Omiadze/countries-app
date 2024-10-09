c457947260bc50927167a53834b3eef0fd74bcfb
Omiadze <teoomiadze18@gmail.com>
2024-10-09 18:59:02 +0400

    use useState for the countries array, add heart Btn and votes, add sort btns

---
+++ dist/assets/index-CWvuiQjW.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-D3DMtK8j.js";const r=()=>e.jsx("div",{children:"favorites page"}),t=()=>e.jsx("div",{children:e.jsx(r,{})});export{t as default};
--- dist/assets/index-gnwt2HWB.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-CO5zazZY.js";const r=()=>e.jsx("div",{children:"favorites page"}),t=()=>e.jsx("div",{children:e.jsx(r,{})});export{t as default};
